3d1fc5eedab79204a8881885d097416665b76e2e
pranav wagh <waghpranav222@gmail.com>
2022-01-22 23:33:11 +0530

    first commit

---
+++ src/test/java/study/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package study;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld()
+	{
+		System.out.println("Hello World !!!");
+	}
+	
+	
+}
